0694f5efbd25702c864e5c8bce5c14f36e1164f2
Matthew Lewandowski <MLewandowski@mac-MLewandowski.local>
2017-05-18 10:24:30 -0700

    initial commit

---
+++ src/app/products/product-list.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+
+import { IProduct } from './product';
+import { ProductService } from './product.service';
+
+@Component({
+  // add a module.id here for relative paths if you want. Then you can remove 'app/products/' from the following urls
+  templateUrl: 'app/products/product-list.component.html',
+  styleUrls: ['app/products/product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+  pageTitle: string = 'Product List';
+  imageWidth: number = 50;
+  imageMargin: number = 2;
+  showImage: boolean = false;
+  listFilter: string;
+  errorMessage: string;
+  products: IProduct[];
+
+  constructor(private _productService: ProductService){
+
+  }
+
+  toggleImage(): void {
+    this.showImage = !this.showImage;
+  }
+
+  ngOnInit(): void {
+    this._productService.getProducts()
+      .subscribe(products => this.products = products, error => this.errorMessage = <any>error);
+  }
+
+  onRatingClicked(message: string): void {
+    this.pageTitle = 'Product List: ' + message;
+  }
+}
---
+++ src/app/products/product.ts
@@ -0,0 +1,10 @@
+export interface IProduct {
+  productId: number;
+  productName: string;
+  productCode: string;
+  inStock: boolean;
+  price: number;
+  description: string;
+  starRating: number;
+  imageUrl: string;
+}
